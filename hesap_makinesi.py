f9f4a2ef56db52674ab27b097aebff5e27d28e8b
Orhun YILDIZ <orhunyyildiz@gmail.com>
2017-08-12 16:58:51 +0300

    Update 2.0

--- hesap_makinesi.py
+++ hesap_makinesi.py
@@ -12,23 +12,31 @@ Hesap Makinesi Programı
 4. Bölme İşlemi
 ********************
 """)
+while True:
 
-sayi1 = int(input("İlk sayıyı giriniz: "))
-sayi2 = int(input("İkinci sayıyı giriniz: "))
+    sayi1 = int(input("İlk sayıyı giriniz: "))
+    sayi2 = int(input("İkinci sayıyı giriniz: "))
 
-işlem = input("İşlemi giriniz: ")
+    işlem = input("İşlemi giriniz: ")
 
-if (işlem == "1"):
-    print("{} + {} = {}".format(sayi1,sayi2,sayi1+sayi2))
+    if (işlem == "1"):
+        print("{} + {} = {}".format(sayi1,sayi2,sayi1+sayi2))
 
-elif(işlem == "2"):
-    print("{} - {} = {}".format(sayi1,sayi2,sayi1-sayi2))
+    elif(işlem == "2"):
+        print("{} - {} = {}".format(sayi1,sayi2,sayi1-sayi2))
 
-elif(işlem == "3"):
-    print("{} * {} = {}".format(sayi1,sayi2,sayi1*sayi2))
+    elif(işlem == "3"):
+        print("{} * {} = {}".format(sayi1,sayi2,sayi1*sayi2))
 
-elif(işlem == "4"):
-    print("{} / {} = {}".format(sayi1,sayi2,sayi1/sayi2))
+    elif(işlem == "4"):
+        print("{} / {} = {}".format(sayi1,sayi2,sayi1/sayi2))
 
-else:
-    print("Geçersiz İşlem")
+    else:
+        print("Geçersiz İşlem")
+
+    cevap = input("Çıkmak için 'q' ya basın. Devam etmek için 'w' ya basın: ")
+
+    if ((cevap == "q") or (cevap == "Q")):
+        break
+    elif ((cevap == "w") or (cevap == "W")):
+        continue
